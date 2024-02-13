af10075cdba1341d30021a90c5f2fbae94000d65
Mr.Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2024-02-13 12:00:58 +0600

    Library app created

---
+++ library-app/src/test/java/kg/alatoo/libraryapp/LibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.alatoo.libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
